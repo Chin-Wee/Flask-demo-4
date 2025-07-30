dde901de052bf553de211e1419120fe10ce2bda9
= <=>
2025-07-30 10:36:02 +0800

    add stuff

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+if __name__ == "__main__":
+    app.run(port = 80, host="0.0.0.0", debug=True)
